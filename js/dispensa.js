4693777f5b69105cb700d56b6e09b4f75766a2ef
gabrielcruzg3 <cruz.gabrielg3@gmail.com>
2021-09-04 11:35:11 -0300

    erro na listagem

--- js/dispensa.js
+++ js/dispensa.js
@@ -10,26 +10,26 @@
         localStorage.setItem('nameHave' + [i], nameHave.value)
         localStorage.setItem('priceHint' + [i], priceHint.value)
         localStorage.setItem('amountHave' + [i], amountHave.value)
-        localStorage.setItem('idHave', [i])
+        // localStorage.setItem('idHave', [i])
     }
 // fim set itensHave //********************************
 
 // logica "Buy"    
-    function setItensBuy(){
-        var barCodeBuy = document.getElementById('barCodeBuy')
-        var nameBuy = document.getElementById('nameBuy')
-        var priceBuy = document.getElementById('priceBuy')
-        var amountBuy = document.getElementById('amountBuy')
-        var store = document.getElementById('store')
-        // var idBuy = document.getElementById('idBuy')
+    // function setItensBuy(){
+    //     var barCodeBuy = document.getElementById('barCodeBuy')
+    //     var nameBuy = document.getElementById('nameBuy')
+    //     var priceBuy = document.getElementById('priceBuy')
+    //     var amountBuy = document.getElementById('amountBuy')
+    //     var store = document.getElementById('store')
+    //     // var idBuy = document.getElementById('idBuy')
 
-        localStorage.setItem('barCodeBuy' + [i], barCodeBuy)
-        localStorage.setItem('nameBuy' + [i], nameBuy)
-        localStorage.setItem('priceBuy' + [i], priceBuy)
-        localStorage.setItem('amountBuy' + [i], amountBuy)
-        localStorage.setItem('store' + [i], store)
-        localStorage.setItem('idBuy' + [i], idBuy)
-    }
+    //     localStorage.setItem('barCodeBuy' + [i], barCodeBuy)
+    //     localStorage.setItem('nameBuy' + [i], nameBuy)
+    //     localStorage.setItem('priceBuy' + [i], priceBuy)
+    //     localStorage.setItem('amountBuy' + [i], amountBuy)
+    //     localStorage.setItem('store' + [i], store)
+    //     localStorage.setItem('idBuy' + [i], idBuy)
+    // }
 //fim itensBuy //********************************
 
 function cadastraProdutoHave(){
@@ -47,62 +47,69 @@ function cadastraProdutoHave(){
         localStorage.setItem('idHave', i)
     }    
 }
-function cadastraProdutoBuy(){
-    if( i == 0){
-        setItensBuy();
-        i++;
-        localStorage.setItem('idBuy', i)
-    }
-    else{
-        setItensBuy();
-        i++;
-        localStorage.setItem('idBuy', i)
-    }
-}
+// function cadastraProdutoBuy(){
+//     if( i == 0){
+//         setItensBuy();
+//         i++;
+//         localStorage.setItem('idBuy', i)
+//     }
+//     else{
+//         setItensBuy();
+//         i++;
+//         localStorage.setItem('idBuy', i)
+//     }
+// }
 
 // listaHave
-var tbodyListaProdutosHave = document.getElementById('listaProdutoHave');
+var tbodyListaProdutosHave = document.querySelector('#listaProdutoHave');
 
 // fim listaHave //********************************
 
 function getItensHave(){
-    let barCodeHaveList = document.getElementById('barCodeHaveList')
-    let nameHaveList = document.getElementById('nameHaveList')
-    let priceHintList = document.getElementById('priceHintList')
-    let amountHaveList = document.getElementById('amountHaveList')
+    let barCodeHaveList = document.getElementById('barCodeHaveList'+ [i])
+    let nameHaveList = document.getElementById('nameHaveList'+ [i])
+    let priceHintList = document.getElementById('priceHintList'+ [i])
+    let amountHaveList = document.getElementById('amountHaveList'+ [i])
     // let idHaveList = document.getElementById('idHaveList')
 
     barCodeHaveList.innerHTML = localStorage.getItem('barCodeHave'+[i])
     nameHaveList.innerHTML = localStorage.getItem('nameHave'+[i])
     priceHintList.innerHTML = localStorage.getItem('priceHint'+[i])
     amountHaveList.innerHTML = localStorage.getItem('amountHave'+[i])
-    // idHaveList
 }
 
 function listaProdutoHave(){
     
-    i = localStorage.getItem('idHave')
+    idHave = localStorage.getItem('idHave')
+    console.log(idHave)
+    i=0;
+    
+    for(let i = 0; i <= idHave; i++){
+        console.log(i)
+        
+        var trListaHave = document.createElement('tr')
+            trListaHave.id = "trListaHave"
+            var trLista = document.querySelector('#trListaHave')
 
-    for (let i = 0; i < i.length; i++){
-        var trLista = document.createElement('tr')
-            trLista.id = "trListaHave"+[i]
+            tbodyListaProdutosHave.appendChild(trListaHave)
 
-        var tdLista = document.createElement('td')
-            tdLista.id = "barCodeHaveList"+[i]
-        var tdLista = document.createElement('td')
-            tdLista.id = "nameHaveList"+[i]
-        var tdLista = document.createElement('td')
-            tdLista.id = "priceHintList"+[i]
-        var tdLista = document.createElement('td')
-            tdLista.id = "amountHaveList"+[i]
+        var barCodeHaveList = document.createElement('td')
+        barCodeHaveList.id = "barCodeHaveList"+ [i]
+            
+        var nameHaveList = document.createElement('td')
+            nameHaveList.id = "nameHaveList"+ [i]
 
-        tbodyListaProdutosHave.appendChild(trLista)
-            trLista.appendChild(tdLista)
+        var priceHintList = document.createElement('td')
+            priceHintList.id = "priceHintList"+ [i]
+            
+        var amountHaveList = document.createElement('td')
+            amountHaveList.id = "amountHaveList"+ [i]
+                        
+        trLista.appendChild(barCodeHaveList, nameHaveList,priceHintList,amountHaveList)
             
         getItensHave();
-    }
-
-    
+        
+    }    
 }
 
 // listaBuy
@@ -110,15 +117,15 @@ function listaProdutoHave(){
 
 //fim listaBuy //********************************
 
-function listaProdutoBuy(){   
+// function listaProdutoBuy(){   
     
     
-    var trLista = document.createElement('tr')
-        trLista.id = "trListaBuy"+[i]
+//     var trLista = document.createElement('tr')
+//         trLista.id = "trListaBuy"+[i]
 
-    var tdLista = document.createElement('td')
-        tdLista.id = "tdListaBuy" + [i]
+//     var tdLista = document.createElement('td')
+//         tdLista.id = "tdListaBuy" + [i]
 
-    tbodyListaProdutos.appendChild(trLista)
-        trLista.appendChild(tdLista)
-}
+//     tbodyListaProdutos.appendChild(trLista)
+//         trLista.appendChild(tdLista)
+// }
